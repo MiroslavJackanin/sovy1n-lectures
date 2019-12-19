0d45da0129ae9ec4d9e0e5eee412524573b9cebf
Miroslav Jackanin <miroslav.jackanin@akademiasovy.sk>
2019-12-19 09:03:53 +0100

    completed task from 19/12/2019

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 import "./styles.css";
-
-console.log("hello");
-fetch("https://swapi.co/api/people/1/").then(function(response) {
-    response.json().then(function(data) {
-        console.log(data);
-        console.log(data.name);
-        document.getElementById("name").innerHTML = data.name;
-    });
-});
